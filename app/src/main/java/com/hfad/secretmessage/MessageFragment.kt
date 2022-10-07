11be82d99c8e727bbc1dcfccfd7c5eb1f199b646
Ryan Henderson <wrhenders@gmail.com>
2022-10-07 11:07:20 -0500

    added encrypt fragment

--- app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
 
@@ -12,7 +14,12 @@ class MessageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val startButton = view.findViewById<Button>(R.id.next)
+
+        startButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+        return view
     }
 }
